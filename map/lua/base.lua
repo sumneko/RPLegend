571eb6186f4bc65678b52b2d97629587f6361358
最萌小汐 <sunyijiusyj@126.com>
2014-05-25 00:24:04 +0800

    hello world!

---
+++ map/lua/base.lua
@@ -0,0 +1,17 @@
+	jass = require 'jass.common'
+	japi = require 'jass.japi'
+	runtime = require 'jass.runtime'
+
+	--打开控制台
+	runtime.console = true
+
+	--设置句柄等级为0(地图中所有的句柄均使用table封装)
+	runtime.handle_level = 0
+
+	--关闭等待功能
+	runtime.sleep = false
+
+	--简化版的require...
+	function need(name)
+		return require('lua\\' .. name .. '.lua')
+	end
